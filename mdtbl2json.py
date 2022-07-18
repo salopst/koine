5d7f844241534c7c7c13435bfe2835774b68176b
Rubinetto <stephen@yearl.us>
2022-07-18 12:44:56 +0100

    Take command line args.

--- mdtbl2json.py
+++ mdtbl2json.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # 2022-07-18T06:57:13+0100
+import getopt, sys
 import json
+from os.path import exists as file_exists
+
 
 koine_table='''| s_no | m_no | Freq. | lexeme     | Gen.. | Art.  | POS       | gloss_en                                                               |
 | ---- | ---- | ----- | ---------- | ----- | ----- | --------- | ---------------------------------------------------------------------- |
@@ -24,4 +27,39 @@ def mdtbl2json(table):
       ret.append({keys[_i]:v.strip() for _i,v in enumerate(l.split('|')) if  _i>0 and _i<len(keys)-1})
   return json.dumps(ret, indent = 2, ensure_ascii=False)
 
-print(mdtbl2json(koine_table))
+def main(argv):
+  inputfile = ''
+  outputfile = ''
+  try:
+     opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+  except getopt.GetoptError:
+     print ('mdtbl3json.py -i <inputfile> -o <outputfile>')
+     sys.exit(2)
+  for opt, arg in opts:
+     if opt == '-h':
+        print ('test.py -i <inputfile> -o <outputfile>')
+        sys.exit()
+     elif opt in ("-i", "--ifile"):
+        inputfile = arg
+     elif opt in ("-o", "--ofile"):
+        outputfile = arg
+  print(f'inputfile: {inputfile}')
+  print(f'outputfile: {outputfile}')
+
+  if file_exists(inputfile):
+    inp = open(inputfile, "r")
+    data = inp.read()
+    print(f'Input: {data}\n=========================')
+    json_data = mdtbl2json(data)
+    print(f'Output: {json_data}\n=========================')
+    with open(outputfile, 'w', encoding='utf-8') as f:
+      json.dump(json_data, f, indent = 2, ensure_ascii=False)
+    inp.close()
+  else:
+    print(f'File {inputfile} does not exist')
+  
+import json
+
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
